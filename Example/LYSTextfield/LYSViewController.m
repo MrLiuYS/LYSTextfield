3afd99161f6ce90d6a2175d21202231a6388c064
刘永生 <LiuYongSheng@liuyongshengdeMacBook-Pro.local>
2017-03-06 14:59:33 +0800

    间隔功能

--- Example/LYSTextfield/LYSViewController.m
+++ Example/LYSTextfield/LYSViewController.m
@@ -8,7 +8,10 @@
 
 #import "LYSViewController.h"
 
-@interface LYSViewController ()
+#import "LYSTextfield.h"
+
+
+@interface LYSViewController () <UITextFieldDelegate>
 
 @end
 
@@ -18,6 +21,26 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    LYSTextfield * textfield = [[LYSTextfield alloc]initWithFrame:CGRectMake(100, 100, 200, 100)];
+    
+    textfield.delegate = self;
+    
+    textfield.maxLimit(50).spacer(@" ").spacerNum(@(3)).spacerNum(@(4)).spacerNum(@(5)).removeSpacerNums();
+    
+    textfield.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:textfield];
+    
+    
+}
+
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    
+    return YES;
+    
 }
 
 - (void)didReceiveMemoryWarning
